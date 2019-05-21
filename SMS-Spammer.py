90e1fca54287e6aa8f9820f2547b1979b5d8722e
STAR-Sec <47953468+STAR-Sec@users.noreply.github.com>
2019-05-21 12:08:17 -0400

    Update SMS-Spammer.py

--- SMS-Spammer.py
+++ SMS-Spammer.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python
 import sys, os
-# Restart ####################
-def restart_program():
-   python = sys.executable
-   os.execl(python, python, * sys.argv)
-   curdir = os.getcwd()
+
 ##############################
 """ Coded By Allistar by Star Sec."""
 os.system("clear")
 print " "
-print "   ___   __  __   ___     ___       "
-print "  / __| |  \/  | / __|   / __|  _ __   __ _   _ __    _ __    ___   _ _ "
-print "  \__ \ | |\/| | \__ \   \__ \ | '_ \ / _` | | '  \  | '  \  / -_) | '_|"
-print "  |___/ |_|  |_| |___/   |___/ | .__/ \__,_| |_|_|_| |_|_|_| \___| |_|  "
-print "                               |_|                                      "
+print "   ___   __  __   ___     ___                                             "
+print "  / __| |  \/  | / __|   / __|  _ __   __ _   _ __    _ __    ___   _ _   "
+print "  \__ \ | |\/| | \__ \   \__ \ | '_ \ / _` | | '  \  | '  \  / -_) | '_|. "
+print "  |___/ |_|  |_| |___/   |___/ | .__/ \__,_| |_|_|_| |_|_|_| \___| |_|    "
+print "                               |_|                                        "
 print "------------------------------------------------------------------------- "
-print "  ===|[ SMS Spamer ]|==="
-print "  [01] Setup New Session        "
+print "  ===|[ SMS Spamer ]|===         "
+print "  [01] Setup New Session         "
 print "  [02] Repeat Last Spam          "
-print "  [03] Update           "
-print "  [00] Exit"
-print
+print "  [03] Update                    "
+print "  [00] Exit                      "
+print "                                 " 
+
 Spammer = raw_input(" Choose:  ")
+while True:
+     if (Spammer == '01' or Spammer == '1'):
+            print "Input Number:"
+            print "SMS Rate: 1s - 10min"
+            Delay = raw_input(" Delay: ")
+            Number = raw_input(" Number: ")
+            Message = raw_input(" Message: ")
+            os.system("watch -n %s termux-sms-send -n %s %s " % (Delay, Number, Message))
+            break
+       
+     elif (Spammer == '02' or Spammer == '2'):
+            print "Coming Soon"
+            break
 
-if (Spammer == '01' or Spammer == '1'):
-       print "Input Number:"
-       print "SMS Rate: 1s - 10min"
-       Delay = raw_input(" Delay: ")
-       Number = raw_input(" Number: ")
-       Message = raw_input(" Message: ")
-       os.system("watch -n %s termux-sms-send -n %s %s " % (Delay, Number, Message))
-       sys.exit()
+     elif (Spammer == '03' or Spammer == '3'):
+            os.system("git clone https://github.com/STAR-Sec/SMS-Spammer")
+            break
+     
+     elif (Spammer == '00' or Spammer == '0'):
+            print "\n[!] Exit the Program..."
+            sys.exit()
+            break
 
-elif (Spammer == '02' or Spammer == '2'):
-       print "Coming Soon"
-
-elif (Spammer == '03' or Spammer == '3'):
-       os.system("git clone https://github.com/STAR-Sec/SMS-Spammer")
-      
-elif (Spammer == '00' or Spammer == '0'):
-       print "\n[!] Exit the Program..."
-       sys.exit()
-
-else:
-       print "\n[!] ERROR : Wrong Input"
-       time.sleep(1)
-       restart_program()
+     else:
+            print "\n[!] ERROR : Wrong Input"
+            os.system("sleep 1")
+       
