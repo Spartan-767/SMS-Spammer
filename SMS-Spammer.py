9937008f542b6420da15dfa019a404920de57c6e
STAR-Sec <47953468+STAR-Sec@users.noreply.github.com>
2019-05-22 08:21:30 -0400

    Update SMS-Spammer.py

--- SMS-Spammer.py
+++ SMS-Spammer.py
@@ -10,12 +10,12 @@ def restart_program():
 """ Coded By Allistar by Star Sec."""
 os.system("clear")
 print " "
-print "   ___   __  __   ___     ___                                             "
-print "  / __| |  \/  | / __|   / __|  _ __   __ _   _ __    _ __    ___   _ _   "
-print "  \__ \ | |\/| | \__ \   \__ \ | '_ \ / _` | | '  \  | '  \  / -_) | '_|. "
-print "  |___/ |_|  |_| |___/   |___/ | .__/ \__,_| |_|_|_| |_|_|_| \___| |_|    "
-print "                               |_|                                        "
-print "------------------------------------------------------------------------- "
+print "\033[1;32;40m   ___   __  __   ___     ___                                             "
+print "\033[1;32;40m  / __| |  \/  | / __|   / __|  _ __   __ _   _ __    _ __    ___   _ _   "
+print "\033[1;32;40m  \__ \ | |\/| | \__ \   \__ \ | '_ \ / _` | | '  \  | '  \  / -_) | '_|. "
+print "\033[1;32;40m  |___/ |_|  |_| |___/   |___/ | .__/ \__,_| |_|_|_| |_|_|_| \___| |_|    "
+print "\033[1;32;40m                               |_|                                        "
+print "\033[1;32;40m------------------------------------------------------------------------- "
 print "  ===|[ SMS Spamer ]|===         "
 print "  [01] Setup New Session         "
 print "  [02] Spam From Contact         "
@@ -38,8 +38,27 @@ while True:
             print "(Add Contact y or n?)"
             Choice = raw_input(">")
             if (Choice == 'Y' or Choice == 'y'):
-                 Name1 = raw_input("Name: ")
-                 Number1 = raw_input("Number: ")
+                 Name1 = Empty
+                 Name2 = Empty
+                 Name3 = Empty
+                 print "Contact 1: " + Name1
+                 print "Contact 2: " + Name2
+                 print "Contact 3: " + Name3 
+                 Num = raw_input("Contact Number: ")
+                 Name + Num = raw_input("Name: ")
+                 Number + Num = raw_input("Number: ")
+                 os.system("clear")
+                 print (""Name%s " (Num)")
+                 Option = raw_input("Is this correct, Y or N?\N")
+                 if (Option == 'Y' or Option == 'y'):
+                      print "Done"
+                      os.system("clear")
+                      sys.exit
+                    
+                 elif (Option == 'N' Option == 'n'):
+                      print "Sorry Please Try again"
+                      sys.exit 
+                    
             elif (Choice == 'N' or Choice == 'n'):
                  print "exiting"
                  break
